60b40695520b6d9761dd6c3c7995922766ada149
Nataly Rojas <natalyrojasm5@gmail.com>
2024-10-12 19:23:47 -0500

    🎨 feat: Structure

---
+++ src/modules/core/layouts/LayoutMain/LayoutMain.tsx
@@ -0,0 +1,13 @@
+export const LayoutMain = ({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) => {
+  return (
+    <>
+      <p>Navbar</p>
+      LayoutMain
+      <p>Footer</p>
+    </>
+  );
+};
