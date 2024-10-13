f15673d9723b082575f2411539292c8147783e0f
Nataly Rojas <natalyrojasm5@gmail.com>
2024-10-13 11:33:57 -0500

    ✨ feat: use  modal password created

--- src/modules/auth/views/new-password/NewPassword.tsx
+++ src/modules/auth/views/new-password/NewPassword.tsx
@@ -1,15 +1,18 @@
 import { fontMonomaniacOne } from "@/modules/core/utils";
-import { FormNewPassword } from "@/modules/auth/components";
+import { FormNewPassword, ModalPasswordCreated } from "@/modules/auth/components";
 
 export const ViewNewPassword = () => {
   return (
-    <section className="text-center">
-      <header
-        className={`${fontMonomaniacOne.className} flex flex-col items-center gap-2 mb-4`}
-      >
-        <h1 className="text-5xl text-white">Nueva Contraseña</h1>
-      </header>
-      <FormNewPassword />
-    </section>
+    <>
+      <section className="text-center">
+        <header
+          className={`${fontMonomaniacOne.className} flex flex-col items-center gap-2 mb-4`}
+        >
+          <h1 className="text-5xl text-white">Nueva Contraseña</h1>
+        </header>
+        <FormNewPassword />
+      </section>
+      <ModalPasswordCreated />
+    </>
   );
 };
