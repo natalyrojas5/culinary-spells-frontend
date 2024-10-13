a5dcce6ed0fbe2b0d5ecab491ff35ecdf3403e70
Nataly Rojas <natalyrojasm5@gmail.com>
2024-10-13 00:27:44 -0500

    ♻️ ref: Refactor Form Forgot Password

--- src/modules/auth/views/forgot-password/ForgotPassword.tsx
+++ src/modules/auth/views/forgot-password/ForgotPassword.tsx
@@ -1,15 +1,9 @@
 "use client";
 
-import { InputWithLabel } from "@/modules/core/components";
-import { BUTTON, PATH } from "@/modules/core/constants";
-import { fontJollyLodger, fontMonomaniacOne } from "@/modules/core/utils";
-import { useRouter } from "next/navigation";
+import { fontMonomaniacOne } from "@/modules/core/utils";
+import { FormForgotPassword } from "@/modules/auth/components";
 
 export const ViewForgotPassword = () => {
-  const {
-    orange: { size, type },
-  } = BUTTON;
-  const router = useRouter();
   return (
     <section className="text-center">
       <header
@@ -18,27 +12,7 @@ export const ViewForgotPassword = () => {
         <h1 className="text-5xl text-white">Olvide Contraseña</h1>
       </header>
 
-      <form className="flex flex-col gap-4 items-center w-[500px] mx-auto mt-8">
-        <InputWithLabel
-          label="Correo electrónico"
-          name="email"
-          type="email"
-          placeholder="Ingresa tu correo electrónico"
-        />
-        <button
-          type="button"
-          className={`${fontMonomaniacOne.className} c-txt-orange text-3xl underline`}
-          onClick={() => router.push(PATH.login)}
-        >
-          Recordé mi contraseña
-        </button>
-        <button
-          type="button"
-          className={`${size.big} ${type.base} ${fontJollyLodger.className} mt-8`}
-        >
-          Solicitar Nueva Contraseña
-        </button>
-      </form>
+      <FormForgotPassword />
     </section>
   );
 };
