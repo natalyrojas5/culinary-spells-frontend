41a61f1d5f9846b00b9362765d240f02536e9930
Nataly Rojas <natalyrojasm5@gmail.com>
2024-10-13 19:08:07 -0500

    ✨ feat(recipes): add componente RecipeCreatorSmall

---
+++ src/modules/recipes/components/RecipeCreatorSmall/RecipeCreatorSmall.tsx
@@ -0,0 +1,31 @@
+import empty from "@/modules/core/assets/empty.jpg";
+
+import { fontMali } from "@/modules/core/utils";
+import Image from "next/image";
+
+interface Props {
+  name: string;
+  country: string;
+  image?: string;
+}
+
+export const RecipeCreatorSmall = ({ name, country, image }: Props) => {
+  return (
+    <div
+      className={`shadow-md border-2 bg-[#2f036712] border-black text-black p-3 rounded-lg mt-4 flex gap-4 items-center ${fontMali.className}`}
+    >
+      <Image
+        src={image ? image : empty}
+        width={200}
+        height={160}
+        alt="Recipe Creator"
+        className="rounded-lg w-[80px] h-[80px] object-cover"
+      />
+      <div>
+        <p className="font-bold">Creador:</p>
+        <p className="text-sm">Nombre: {name}</p>
+        <p className="text-sm">País: {country}</p>
+      </div>
+    </div>
+  );
+};
