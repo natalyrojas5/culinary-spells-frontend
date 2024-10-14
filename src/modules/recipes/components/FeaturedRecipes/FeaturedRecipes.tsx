7968f15e87e306c8bc0a78b7fc16c40f5c21534e
Nataly Rojas <natalyrojasm5@gmail.com>
2024-10-13 19:07:50 -0500

    ✨ feat(recipes): add componente FeaturedRecipes

---
+++ src/modules/recipes/components/FeaturedRecipes/FeaturedRecipes.tsx
@@ -0,0 +1,19 @@
+import { Recipe } from "@/modules/core/components";
+import { fontMonomaniacOne } from "@/modules/core/utils";
+
+export const FeaturedRecipes = () => {
+  return (
+    <section className="mt-10">
+      <h2
+        className={`text-3xl c-txt-orange ${fontMonomaniacOne.className} mb-6`}
+      >
+        Recetas Destacadas
+      </h2>
+      <section className="grid grid-cols-3 gap-6">
+        <Recipe isHighlighted />
+        <Recipe isHighlighted />
+        <Recipe isHighlighted />
+      </section>
+    </section>
+  );
+};
