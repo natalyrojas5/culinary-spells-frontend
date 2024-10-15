2366f55dbaf6f89eb170727588dc4bd66bf785a8
Nataly Rojas <natalyrojasm5@gmail.com>
2024-10-14 21:54:11 -0500

    ✨ feat: remove btn goback

--- src/modules/recipes/views/my-recipes/MyRecipes.tsx
+++ src/modules/recipes/views/my-recipes/MyRecipes.tsx
@@ -1,20 +1,17 @@
-import { GoBack, InputSearch } from "@/modules/core/components";
+import { InputSearch } from "@/modules/core/components";
 import { fontMonomaniacOne } from "@/modules/core/utils";
 import { MyRecipes } from "@/modules/recipes/components";
 
 export const ViewMyRecipes = () => {
   return (
-    <>
-      <GoBack />
-      <main className="flex flex-col items-center gap-3 w-full">
-        <h1
-          className={`font-extrabold text-6xl c-txt-golden-yellow mb-6 ${fontMonomaniacOne.className}`}
-        >
-          Mis Receta
-        </h1>
-        <InputSearch />
-        <MyRecipes />
-      </main>
-    </>
+    <main className="flex flex-col items-center gap-3 w-full">
+      <h1
+        className={`font-extrabold text-6xl c-txt-golden-yellow mb-6 ${fontMonomaniacOne.className}`}
+      >
+        Mis Receta
+      </h1>
+      <InputSearch />
+      <MyRecipes />
+    </main>
   );
 };
