4fcdfa46feedffeb2840c1272e5d986b0d49c06a
Nataly Rojas <natalyrojasm5@gmail.com>
2024-10-15 18:39:12 -0500

    ✨ feat: Update Footer

--- src/modules/core/components/Footer/Footer.tsx
+++ src/modules/core/components/Footer/Footer.tsx
@@ -2,13 +2,13 @@ import { fontJollyLodger } from "@/modules/core/utils";
 
 export const Footer = () => {
   return (
-    <div
-      className={`${fontJollyLodger.className} bg-purple-700 p-6 text-center flex gap-2 flex-col  justify-center border-t-8 c-border-golden-yellow`}
+    <footer
+      className={`${fontJollyLodger.className} bg-purple-700 p-6 text-center flex gap-2 flex-col  justify-center border-t-8 c-border-golden-yellow w-full `}
     >
       <p className="c-txt-golden-yellow text-3xl">
         Desarrollador con 💜 para Hackathon Cloudinary
       </p>
       <p className="text-white text-2xl">Spellbound Coders</p>
-    </div>
+    </footer>
   );
 };
