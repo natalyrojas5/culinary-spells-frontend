176cf8c2c1de2c193576038205c6a01085406d9a
Nataly Rojas <natalyrojasm5@gmail.com>
2024-10-16 20:12:46 -0500

    ✨ feat: protected page recipes

--- src/modules/recipes/views/recipes/Recipes.tsx
+++ src/modules/recipes/views/recipes/Recipes.tsx
@@ -7,16 +7,24 @@ import {
   Recipes,
 } from "@/modules/recipes/components";
 import { getRecipes } from "@/modules/recipes/actions";
+import { getServerSession } from "next-auth/next";
+import { authConfig } from "@/modules/core/utils";
 
 export const ViewRecipes = async () => {
+  const session = await getServerSession(authConfig);
+  const isLogged = !!session?.user;
   const { data } = (await getRecipes()) || { data: [] };
 
   return (
     <>
       <Banner />
-      <FeaturedRecipe />
-      <FeaturedRecipes />
-      <Recipes recipes={data} />
+      {isLogged && (
+        <>
+          <FeaturedRecipe />
+          <FeaturedRecipes />
+        </>
+      )}
+      <Recipes recipes={isLogged ? data : data.slice(0, 3)} />
       <AddRecipe />
       <ModalAccessRecipe />
     </>
