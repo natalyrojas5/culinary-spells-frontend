a1a1c0470803aaac4930c3d8cc14206c03f4ef9c
Nataly Rojas <natalyrojasm5@gmail.com>
2024-10-19 21:41:56 -0500

    ✨ feat: Add component NotFound

---
+++ src/modules/core/components/NotFound/NotFound.tsx
@@ -0,0 +1,25 @@
+import Image from "next/image";
+import { fontMali } from "../../utils";
+import avatarSad from "@/modules/core/assets/avatar-sad.png";
+
+export const NotFound = () => {
+  return (
+    <section
+      className={`p-4 bg-purple-700 ${fontMali.className} flex gap-4 w-full items-center rounded-lg`}
+    >
+      <Image
+        src={avatarSad}
+        width={300}
+        height={300}
+        className="w-[240px]"
+        alt="Not Found"
+      />
+      <div className="text-white flex flex-col gap-3">
+        <h2 className="text-3xl  font-bold">
+          Oops! No encontramos resultados.
+        </h2>
+        <p className="text-xl">Aún no hay datos disponibles.</p>
+      </div>
+    </section>
+  );
+};
