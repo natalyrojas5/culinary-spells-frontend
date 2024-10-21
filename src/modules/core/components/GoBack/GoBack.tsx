d342af0b1d4f6f4a1aa5e7a91ca78f3bfad04c74
Nataly Rojas <natalyrojasm5@gmail.com>
2024-10-20 23:39:38 -0500

    💄 feat: changes UI (#33)

--- src/modules/core/components/GoBack/GoBack.tsx
+++ src/modules/core/components/GoBack/GoBack.tsx
@@ -5,21 +5,32 @@ import { fontJollyLodger } from "@/modules/core/utils";
 import { useRouter } from "next/navigation";
 import { IoIosArrowBack } from "react-icons/io";
 
-export const GoBack = () => {
+interface IProps {
+  text?: string;
+  path?: string;
+}
+export const GoBack = ({ text = "Regresar", path = "" }: IProps) => {
   const {
     purpleDark: { size, type },
   } = BUTTON;
 
   const router = useRouter();
+  const goBack = () => {
+    if (path) {
+      router.push(path);
+    } else {
+      router.back();
+    }
+  };
   return (
     <button
       type="button"
       className={`
       ${size.big} ${type.base} ${fontJollyLodger.className}  flex gap-2 items-center`}
-      onClick={() => router.back()}
+      onClick={goBack}
     >
       <IoIosArrowBack size={45} />
-      Regresar
+      {text}
     </button>
   );
 };
