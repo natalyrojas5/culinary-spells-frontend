93fee3d844624a7078e859f7c27c4d6a16590d9c
Nataly Rojas <natalyrojasm5@gmail.com>
2024-10-22 17:57:59 -0500

    ✨ feat: changes

--- src/modules/core/layouts/LayoutMain/LayoutMain.tsx
+++ src/modules/core/layouts/LayoutMain/LayoutMain.tsx
@@ -2,12 +2,19 @@
 
 import { Navigation, Footer, Toast } from "@/modules/core/components";
 import { SessionProvider } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
 export const LayoutMain = ({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) => {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push('https://culinary-spells-frontend.netlify.app'); 
+  }, [router]);
   return (
     <SessionProvider>
       <Navigation />
